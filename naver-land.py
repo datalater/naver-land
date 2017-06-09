cb5a6cdf07d9c1e685a2c8e54166c82475de113b
datalater <the7mincheol@gmail.com>
2017-06-09 20:50:39 +0900

    부산시 행정구역 regionCode dict 추가

--- naver-land.py
+++ naver-land.py
@@ -8,15 +8,39 @@ import csv
 
 
 # land.naver.com '부산시 사하구' 검색 후 단지 정보 크롤링
+
+
 def get_list():    
     # query_text = input(">>> 검색할 행정구역을 입력하세요. ex. 부산시 사하구: ")
     
     for page in count(1):
         page_url = "http://land.naver.com/search/complexSearch.nhn"  
+
+        pusan_dict = {
+            '강서구' : 2644000000,
+            '금정구' : 2641000000,
+            '기장군' : 2671000000,
+            '남구' : 2629000000,
+            '동구' : 2617000000,
+            '동래구' : 2626000000,
+            '부산진구' : 2623000000,
+            '북구' : 2632000000,
+            '사상구' : 2653000000,
+            '사하구' : 2638000000,
+            '서구' : 2614000000,
+            '수영구' : 2650000000,
+            '연제구' : 2647000000,
+            '영도구' : 2620000000,
+            '중구' : 2611000000,
+            '해운대구' : 2635000000,
+        }
+
+        query_text = input("부산시 내 행정구역을 입력하세요. ex. 강서구 : ")
+
         params = {
             'complexSearchSortOption':'popularRank.asc',
             'cortarInfo':'[object Object]',
-            'regionAddress':'부산시 사하구',
+            'regionAddress':'부산시 '+ query_text,
             'keywordInfo':'[object Object]',
             'flashData':'[object Object]',
             'flashMapRegionType':'region3',
@@ -29,7 +53,7 @@ def get_list():
             'resultCode':'S00',
             'isFirstSiteArticle':'true',
             'maxSize':0,
-            'regionCode':2638000000,
+            'regionCode': pusandict[query_text],
             'isComplexRegion':'false',
             'isaleComplexCount':0,
             'complexCount':0,
@@ -72,7 +96,7 @@ def get_list():
                 #     # spamwriter.writerow(['Spam', 'Lovely Spam', 'Wonderful Spam'])
                 #     spamwriter.writerows([r_dict['complexTypeName'], r_dict['address'], r_dict['complexName']])
             
-                with open('naver-land-txt.txt', 'a', newline='') as f:
+                with open('naver-land-'+query_text+'.txt, 'a', newline='') as f:
                     # f.write(",".join(row_list))
                     f.write(data)
 
